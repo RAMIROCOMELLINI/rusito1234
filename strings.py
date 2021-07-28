bf56e80a1d22500d4099f03a67edc0fcfc5be31d
RAMIROCOMELLINI <88115957+RAMIROCOMELLINI@users.noreply.github.com>
2021-07-28 19:36:15 -0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,18 @@
+myStr = "Hello World"
+dir(myStr)
+
+#print(myStr.upper())
+#myStr.swapcase()
+#myStr.replace("Hello", "bye")
+#myStr.count('l')
+#myStr.startswith("He")
+#myStr.endswith("ld")
+#myStr.split("o")
+#myStr.find('o')
+#myStr.find("H")
+#len(myStr)
+#myStr.index("e")
+#myStr.isnumeric()
+myStr[-5]
+print("Mi name is " + myStr)
+ 
